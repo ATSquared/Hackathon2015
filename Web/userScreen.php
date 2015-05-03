a486e6ced5bbeeec882071066d1c0ca7305b8955
ptrckCarroll <ptrckalexcarroll@gmail.com>
2015-05-03 06:39:22 -0500

    adding some validation

--- Web/userScreen.php
+++ Web/userScreen.php
@@ -1,73 +1,64 @@
-﻿<?php
+﻿<!DOCTYPE html>
+<?php
 	//connect to db
 	if(isset($_POST['submit'])){
-		//var_dump($_POST);
 		$date = $_POST['date'];
 		$hours = $_POST['hours'];
 		$minutes = $_POST['minutes'];
 		$amPm = $_POST['amPm'];
+		
+		if($amPm == "pm"){
+			$hours += 12;
+		}
+		else if($hours < 10){
+			$hours = "0".$hours;
+		}
+		$date = implode("-", explode("/", $date));
+		$date .= "T$hours:$minutes:00";
+		$address = $_POST['address'];
 		$city = $_POST['city'];
 		$state = $_POST['state'];
 		$zip = $_POST['zip'];
-		$capactiy = $_POST['capacity'];
+		$duration = $_POST['duration'];
+		$capacity = $_POST['capacity'];
 		$comments = $_POST['comments'];
 		
-		$url = 'http://hacktest2015.azurewebsites.net/WcfDataService1.svc/PickupRequests';
-		//$data = array('key1' => 'value1', 'key2' => 'value2');
+		var_dump($date);
 
 		//create codeblock
+		$url = 'http://hacktest2015.azurewebsites.net/WcfDataService1.svc/PickupRequests';
 		$options = array(
 			'http' => array(
 				'header'  => "Content-type: application/json",
 				'method'  => 'POST',
-				'content' => "{ 'PickupDate':'2015-05-02T15:00:00','Address':'1647 S. Blue Island Ave.','City':'Chicago','State':'Illinois','ZIP':'60608','Duration':'3.50','PassengerCount':0}",
+				//'content' => "{ 'PickupDate':'2015-05-02T15:00:00','Address':'1647 S. Blue Island Ave.','City':'Chicago','State':'Illinois','ZIP':'60608','Duration':'3.50','PassengerCount':0}",
+				//'content' => "{'PickupDate':'$date','Address':'$address','City':'$city','State':'$state','ZIP':'$zip','Duration':'$duration','PassengerCount':$capacity}"
+				'content' => "{ \"PickupDate\":\"2015-05-02T15:00:00\",\"Address\":\"1647 S. Blue Island Ave.\",\"City\":\"Chicago\",\"State\":\"Illinois\",\"ZIP\":\"60608\",\"Duration\":\"3.50\",\"PassengerCount\":0}",
 			),
 		);
 		$context  = stream_context_create($options);
 		$result = file_get_contents($url, false, $context);
 
-		var_dump($result);
+		//var_dump($result);
+		//var_dump($options['http']);
 		//end of create
 		
+		
 		//read codeblock
+		/*
 		$test = file_get_contents('http://hacktest2015.azurewebsites.net/WcfDataService1.svc/PickupRequests');
 		$xml = simplexml_load_string($test);
 		foreach($xml->entry as $entry)
-		var_dump($entry->content->children("m", true)->children("d",true) );
-		//end of read
-		/*
-		$sxe = new SimpleXMLElement($test);
-		$namespaces = $sxe->getNamespaces(true);
-		var_dump($namespaces);
-		
-		$json = json_encode($xml, JSON_PRETTY_PRINT, 100);
-		$array = json_decode($json,TRUE);
-		//var_dump($json);
-		
-		//var_dump(array_keys($array['entry'][0]['id']));
-		var_dump($array['entry'][0]['content']);
-		
+			var_dump($entry->content->children("m", true)->children("d",true) );
 		*/
-		
-		//$r = new HttpRequest('http://hacktest2015.azurewebsites.net/WcfDataService1.svc/PickupRequests', HttpRequest::METH_POST);
-		//$r->setOptions(array('cookies' => array('lang' => 'de')));
-		//$r->addPostFields(array('user' => 'mike', 'pass' => 's3c|r3t'));
-		//$r->addPostFile('image', 'profile.jpg', 'image/jpeg');
-		/*
-		try {
-			echo $r->send()->getBody();
-		} catch (HttpException $ex) {
-			echo $ex;
-		}
-		*/
-		
+		//end of read	
 	}//end of if submit pressed
 	
 	
 	
 	
 	?>
-<!DOCTYPE html>
+
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -84,18 +75,33 @@
 	<link rel="stylesheet" href="style/jquery-ui-1.11.4.custom/jquery-ui.css">
 	<link rel="stylesheet" href="style/jquery-ui-1.11.4.custom/jquery-ui.min.css">
 	
-	<!--chosen-->
-	<!--<link rel="stylesheet" href="style/chosen_v1.4.2/chosen.css">-->
-	<!--<script src="style/chosen_v1.4.2/chosen.jquery.js"></script>-->
-	<!--<script src="style/chose_v1.4.2/chosen.jquery.min.js"></script>-->
-	
-	
 	<script type="text/javascript">
 		$(function e(){
 			$("#date").datepicker();
-			//$("select").chosen();
-			//$("select").selectmenu();
+			$("input").blur(function e(){
+				if(!($(this).val() != ""))	
+					$(this).focus();
+			});//end of input on blur
+			
+			$('#zip').blur(function e(){
+				var pattern = /\d{6}/;
+				if(!pattern.test($(this).val()){
+					$(this).value = "Please enter a six digit number";
+					$(this).focus();
+				}
+			});//end of zip on blur
+			
+			//$('#submit').submit(function e(){
+								
+			//});//end of on submit
 		})//end of document.ready
+		
+
+		/*
+		validate(var inputValue){
+			if()
+		}
+	*/
 	</script>	
     <!-- Bootstrap -->
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
@@ -154,18 +160,18 @@
 		   <!--address-->
 		   <div class="row">
 			  <div class="form-group col-md-3">
-				<label for="address" class="col-md-2 control-label" >Address</label>
+				<label for="address" class="control-label" >Address</label>
 					<input type="text" class="form-control" id="address" name="address" placeholder="Address">
 				</div>
 			  <!--City-->
 				<div class="form-group col-md-3">
-					<label for="city" class="col-md-2 control-label" >City</label>
+					<label for="city" class="control-label" >City</label>
 						<input type="text" class="form-control" id="city" name="city" placeholder="city">
 				</div>
 			<!--end of city-->
 			  <!--State-->
 				<div class="form-group col-md-3">
-					<label for="state" class="col-md-2 control-label" >State</label>
+					<label for="state" class="control-label" >State</label>
 							<select id="state" name="state" class="form-control" >
 								<option value="">Select a State</option>
 								<option value="AL">Alabama</option>
@@ -223,16 +229,26 @@
 				<!--end of state-->
 			  <!--zip-->
 				<div class="form-group col-md-3">
-					<label for="zip" class="col-md-2 control-label" >Zip</label>
+					<label for="zip" class="control-label" >Zip</label>
 						<input type="text" class="col-md-3 form-control" id="zip" name="zip" placeholder="Zip" size="6" maxlength="6">
 						<!--<input type="text" class="form-control" id="zip" name="zip" placeholder="Zip" pattern=".d{6}">-->
 					</div>
 					<!--end of zip-->
 			</div><!--end of row-->
+			<div class="row">
+			  <div class="form-group col-md-6">
+					<label for="duration" class="control-label">Duration</label>
+								<select id="duration" name="duration" class="form-control">
+							<?php
+								for($i = 1; $i <= 5; $i++)
+									echo "<option>$i<//option>";
+							?>
+						</select>
+					</div>
 			  <!--Passengers-->
-			  <div class="row">
-				  <div class="form-group col-md-5">
-					<label for="capacity" class="col-md-5 control-label"># of Passengers</label>
+			  
+				  <div class="form-group col-md-6">
+					<label for="capacity" class="control-label"># of Passengers</label>
 								<select id="capacity" name="capacity" class="form-control">
 							<?php
 								for($i = 1; $i <= 20; $i++)
@@ -245,7 +261,7 @@
 			  <div class="row">
 				<!--comments-->
 				<div class="form-group col-md-12">
-					<label for="comments" class="col-md-5 control-label">Comments</label>	
+					<label for="comments" class="control-label">Comments</label>	
 						<textarea class="form-control" name="comments" rows="3" placeholder="Comments"></textarea>
 				</div>
 					<!--end of comments-->
