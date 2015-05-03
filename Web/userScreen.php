85ea1f1e33b04a098d703a51ca3df8e7daab9d84
ptrckCarroll <ptrckalexcarroll@gmail.com>
2015-05-02 22:49:36 -0500

    userScreen php

--- Web/userScreen.php
+++ Web/userScreen.php
@@ -17,8 +17,7 @@
 	<script type="text/javascript">
 		$(function e(){
 			$("#date").datepicker();
-		}//end of document.ready
-		)
+		})//end of document.ready
 	</script>	
     <!-- Bootstrap -->
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
@@ -35,21 +34,36 @@
 	   <!--date-->
 		<div class="form-group">
 			<label for="date" class="col-md-2 control-label" >Date</label>
-			<div class="col-md-10">
+			<div class="col-md-2">
 				<input type="text" class="form-control" id="date" placeholder="Date">
 			</div>
-	  </div>
+	  </div><!--end of date-->
 	  <!--time-->
 		<div class="form-group">
-			<label for="time" class="col-mid-2 control-label">Time</label>
-			<div class="col-md-10">
-				<select class="form-control">
+			<label for="time" class="col-md-2 control-label">Time</label>
+			<div class="col-md-2">
+				<select id="time" class="form-control">
 					<?php
-						for($i = 1; i <= 12; $i++)
+						for($i = 1; $i <= 12; $i++)
 							echo "<option>$i<//option>";
 					?>
 				</select>
-			</div>
+			</div><!--hours-->
+			<div class="col-md-2">
+				<select class="form-control">
+					<option value = "00">00</option>
+					<option value= "15">15</option>
+					<option value= "30">30</option>
+					<option value= "45">45</option>
+				</select>
+			</div><!--minutes-->
+			<div class="col-md-2">
+				<select class="form-control">
+					<option value="am">AM</option>
+					<option value="pm">PM</option>
+				</select>
+			</div><!--am, pm-->
+		</div><!--end of time-->
 	   <h2>Location</h2>
 	   <!--address-->
 		  <div class="form-group">
@@ -57,38 +71,36 @@
 			<div class="col-md-10">
 				<input type="text" class="form-control" id="address" placeholder="Address">
 			</div>
-		  </div>
+		  </div><!--end of address-->
 		  <!--City-->
 		    <div class="form-group">
 				<label for="city" class="col-md-2 control-label" >City</label>
 				<div class="col-md-10">
 					<input type="text" class="form-control" id="city" placeholder="city">
 				</div>
-			</div>
+			</div><!--end of city-->
 		  <!--State-->
 			<div class="form-group">
 				<label for="state" class="col-md-2 control-label" >State</label>
 				<div class="col-md-10">
 					<input type="text" class="form-control" id="state" placeholder="State">
 				</div>
-			</div>
+			</div><!--end of state-->
 		  <!--zip-->
 			<div class="form-group">
 				<label for="zip" class="col-md-2 control-label" >Zip</label>
 				<div class="col-md-10">
 					<input type="text" class="form-control" id="zip" placeholder="Zip">
 				</div>
-			</div>
+			</div><!--end of zip-->
 		  <!--Passengers-->
 		  <div class="form-group">
 		    <label for="numOfPass" class="col-md-2 control-label">Number of Passengers</label>
-		    <div class="col-md-10">
+		    <div class="col-md-2">
 		    	<input type="text" class="form-control" id="numOfPass" placeholder="Number of Passengers">
 		    </div>
-		  </div>
+		  </div><!--end of passengers-->
 		</form>
 	</div><!--end of container-->
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>-->
-  </body>
+</body>
 </html>
